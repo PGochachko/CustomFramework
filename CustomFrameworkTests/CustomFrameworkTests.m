d9575b14a565594a767b343ae61aaf571312aba1
Pavel <pgochachko@yandex.ru>
2024-10-10 14:15:45 +0300

    Init commit

---
+++ CustomFrameworkTests/CustomFrameworkTests.m
@@ -0,0 +1,36 @@
+//
+//  CustomFrameworkTests.m
+//  CustomFrameworkTests
+//
+//  Created by Павел Гочачко on 10.10.2024.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomFrameworkTests : XCTestCase
+
+@end
+
+@implementation CustomFrameworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
